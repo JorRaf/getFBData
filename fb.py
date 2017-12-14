810d9f069b1623329980555e03075ef8addc4572
Jorge Rafael <jorrafoliver@gmail.com>
2017-12-13 21:55:47 -0300

    Update fb.py

--- fb.py
+++ fb.py
@@ -2,21 +2,15 @@
 import urllib2
 import json
 import pymongo
+
 from pymongo import MongoClient
-
-client = MongoClient()
-
-db = client.test_database
 from datetime import datetime
-
 
 dados_json  =   open('config.json','r')
 config =   json.load(dados_json)
-
 api_version = config['api_version']
 page_target = config['page_target']
 data_type   = config['data_type']
 token       = config['token']
 graph_url = config['graph_url'] + api_version +"/" + page_target +"/" + data_type + "?access_token=" + token
-
-result = db.FBComments.insert_one(json.loads(urllib2.urlopen(graph_url).read()))
+result = MongoClient().client.test_database.FBComments.insert_one(json.loads(urllib2.urlopen(graph_url).read()))
